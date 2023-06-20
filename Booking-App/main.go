82c201dbea4a1f382e2ff5b45101b2add203c4f1
Mukesh <mukeshmahato2618@gmail.com>
2023-06-20 23:30:13 +0545

    printing slice lengt

--- Booking-App/main.go
+++ Booking-App/main.go
@@ -38,6 +38,7 @@ func main() {
 	fmt.Printf("The whole slice %v\n", bookings)
 	fmt.Printf("The first value : %v\n", bookings[0])
 	fmt.Printf("Slice type : %T\n", bookings)
+	fmt.Printf("Slice length : %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets, You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
