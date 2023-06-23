ec374c21b5a325248b79a1aa80c6f01fbf8a1594
Mukesh <mukeshmahato2618@gmail.com>
2023-06-23 09:27:37 +0545

    Handling time in golang

--- Let's go with golang/05myTime.go
+++ Let's go with golang/05myTime.go
@@ -8,8 +8,12 @@ import (
 func main() {
 	fmt.Println("Welcome to time study of golang")
 
-	presentTIme := time.Now()
-	fmt.Println(presentTIme)
+	presentTime := time.Now()
+	fmt.Println(presentTime)
 
-	// fmt.Println(presentTime.Format(01-02-2006 Monday))
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createDate := time.Date(2020, time.May, 12, 23, 23, 0, 0, time.UTC)
+	fmt.Println(createDate)
+	fmt.Println(createDate.Format("01-02-2006 Monday"))
 }
