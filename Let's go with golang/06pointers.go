ee8d6febea352d3f40e5124d9e4a99c8ed81c50a
Mukesh <mukeshmahato2618@gmail.com>
2023-06-25 16:49:45 +0545

    updating pointers

--- Let's go with golang/06pointers.go
+++ Let's go with golang/06pointers.go
@@ -16,4 +16,15 @@ func main() {
 
 	*ptr = *ptr + 2
 	fmt.Println("New value is ", num) //New value is  100
+
+	data := []int{1, 2, 3, 4, 5}
+	processLargeData(&data)
+	fmt.Println(data)
+}
+
+/* Efficiently passing large data structures: Pointers are commonly used to avoid unnecessary copying of large data structures when passing them as function arguments. Instead of passing the entire data structure, you can pass a pointer to it, allowing functions to directly modify the original data.*/
+
+func processLargeData(data *[]int) {
+	// Modify data directly
+	(*data)[0] = 10
 }
