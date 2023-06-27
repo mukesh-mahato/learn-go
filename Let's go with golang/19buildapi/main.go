dfbf32a856d81f4bc4d7e568136c09aa44708c22
Mukesh <mukeshmahato2618@gmail.com>
2023-06-27 23:02:23 +0545

    add a course based on request

--- Let's go with golang/19buildapi/main.go
+++ Let's go with golang/19buildapi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -43,4 +44,21 @@ func getAllCources(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all cources")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(coueses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	// loop through courses, find matching id and return the response
+	for _, course := range Course {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course found with given id")
 }
