8ee21d2f4946fc85c417a065040c7c42479966fa
Mukesh <mukeshmahato2618@gmail.com>
2023-06-29 11:07:25 +0545

    updating pointers

--- Let's go with golang/06pointers.go
+++ Let's go with golang/06pointers.go
@@ -20,6 +20,19 @@ func main() {
 	data := []int{1, 2, 3, 4, 5}
 	processLargeData(&data)
 	fmt.Println(data)
+
+	var a int = 42
+	var b *int = &a
+	fmt.Println(a, *b) //42 42
+	a = 25
+	fmt.Println(a, *b) //25 25
+	*b = 15
+	fmt.Println(a, *b) //15 15
+
+	x := [3]int{1, 2, 3}
+	y := &x[0]
+	z := &x[1]
+	fmt.Printf("%v %p %p\n", x, y, z) //[1 2 3] 0xc00001c1f8 0xc00001c200
 }
 
 /* Efficiently passing large data structures: Pointers are commonly used to avoid unnecessary copying of large data structures when passing them as function arguments. Instead of passing the entire data structure, you can pass a pointer to it, allowing functions to directly modify the original data.*/
