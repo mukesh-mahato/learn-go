bb4c1fe9ce3cacd5172c640ce1b32d9ad0f5684f
Mukesh <mukeshmahato2618@gmail.com>
2023-07-09 14:20:02 +0545

    Introduction to concurrency

---
+++ Concurrency/fork-join/no-join-point/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go work() //fork point
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("done waiting, main exit")
+	// join point
+}
+
+func work() {
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("Printing some stuff...")
+}
